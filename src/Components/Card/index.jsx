3045716ea63d9ee6940816af348ae5c100aab516
Bushogun <sotto_j@hotmail.com>
2023-08-28 23:28:32 -0500

    partial: adding delete in checkoutSideMenu

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { PlusIcon } from '@heroicons/react/24/solid'
+import { PlusIcon, CheckIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext }  from '../../Context'
 
 const Card = (data) => {
@@ -16,7 +16,24 @@ const Card = (data) => {
         context.setCartProducts([...context.cartProducts, productData])
         context.openCheckoutSideMenu()
         context.closeProductDetail()
-        console.log('CART ', context.cartProducts)
+        // console.log('CART ', context.cartProducts)
+    }
+
+    const renderIcon = (id) => {
+        const isInCart = context.cartProducts.filter(product => product.id === id).length > 0
+        if (isInCart) {
+            return (
+            <div className='absolute top-0 text-sm right-0 flex justify-center items-center bg-green-300 w-6 h-6 rounded-full m-2'>
+                <CheckIcon className='h-5 w-5 text-black'/>
+            </div>
+                )
+        }else{
+            return (
+            <div className='absolute top-0 text-sm right-0 flex justify-center items-center bg-yellow-300 w-6 h-6 rounded-full m-2' 
+                onClick={(event) => addProductsToCart(event, data.data)}>
+                <PlusIcon className='h-5 w-5 text-black'/>
+            </div>
+        )}
     }
 
     return(
@@ -26,10 +43,7 @@ const Card = (data) => {
             <figure className='relative mb-2 w-full h-4/5'>
                 <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-sm m-2 px-3 py-0.5'>{data.data.category}</span>
                 <img className='w-full h-full object-cover rounded-lg' src={data.data.image} alt={data.data.title}/>
-                <div className='absolute top-0 text-sm right-0 flex justify-center items-center bg-yellow-300 w-6 h-6 rounded-full m-2' 
-                     onClick={(event) => addProductsToCart(event, data.data)}>
-                   <PlusIcon className='h-5 w-5 text-black'/>
-                </div>
+                { renderIcon(data.data.id) }
             </figure>
             <p className='flex justify-between'>
                 <span className='text-sm font-light p-1 m-2' style={{ overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}>{data.data.title}</span>
--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -7,6 +7,10 @@ import './styles.css'
 const CheckoutSideMenu = () =>{
     const context = useContext(ShoppingCartContext)
     // console.log('PRODUCT TO SHOW ', context.productToShow)
+    const handleDelete = (id) => {
+        const filteredProducts = context.cartProducts.filter(product => product.id != id)
+        context.setCartProducts(filteredProducts)
+    }
 return(
     <aside 
         className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black bg-white rounded`}>
@@ -17,14 +21,16 @@ return(
                 onClick={()=>context.closeCheckoutSideMenu()}/>
             </div>
         </div>
-        <div className='px-6'>
+        <div className='px-6 overflow-y-scroll'>
         {
             context.cartProducts.map((product) => (
                 <OrderCard 
                 key={product.id}
+                id={product.id}
                 title={product.title}
                 imageUrl={product.image}
                 price={product.price}
+                handleDelete={handleDelete}
                 />
             ))
         }
